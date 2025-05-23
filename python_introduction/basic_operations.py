322812f62b98c06a75ec56356934d0cf113fc736
Oghenetejiri Enaibe <enaibs99@gmail.com>
2025-05-23 12:29:55 +0100

    added python_introduction and basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+# print(Addition sum) 
+sub = number1 - number2
+# print(Subtraction sub)
+mul = number1 * number2
+print(f"Addition of {number1} and {number2} is", sum)
+print(f"Multiplication of {number1} and {number2} is", mul)
+print(f"Substraction of {number1} and {number2} is", sub)
